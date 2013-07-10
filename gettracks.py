db389159adbb059d2f7ea025044966687bec63d6
Guy John <guy@rumblesan.com>
2013-07-10 22:21:15 +0100

    main script downloads random file from top100

--- gettracks.py
+++ gettracks.py
@@ -1,42 +1,68 @@
 #! /usr/bin/env python
 
-import json
-import urllib
+import ConfigParser
+import argparse
+from datetime import datetime
+
+from jamendoapi import jamendo
+
+import random
+
+
+def parseArgs():
+    parser = argparse.ArgumentParser(description='Stretch track for Tumblr')
+    parser.add_argument('config', help='The config file')
+    args = parser.parse_args()
+
+    config = ConfigParser.SafeConfigParser()
+    config.read(args.config)
+
+    return (args, config)
 
 
 def main():
-    top100 = getTop100()
 
-    first = top100[0]
-    downloadTrack(first["id"], first["name"])
+    print('###################')
+    print('#      Dronr      #')
+    print('###################')
+    print('\n')
+    print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    args, config = parseArgs()
+    #consumerKey = config.get('consumer', 'key')
+    #consumerSecret = config.get('consumer', 'secret')
+    #oauthToken = config.get('oauth', 'key')
+    #oauthSecret = config.get('oauth', 'secret')
+
+    jamendo_client_id = config.get('jamendo', 'clientid')
+
+    j = jamendo(jamendo_client_id)
+
+    top100_args = {
+        "limit": 100,
+        "order": "popularity_week",
+        "ccsa": 1
+    }
+    top100 = j.get("tracks", top100_args)
+
+    track = random.choice(top100)
+
+    print(track)
+
+    download_track(j, track["id"], track["name"], "ogg")
 
 
-def getTop100():
-    jamendoApi = "http://api.jamendo.com/en/get2/id+name/track/json/"
-    topArgs = {"n": 100, "order": "ratingday_desc"}
-    top100Url = jamendoApi + "?" + urllib.urlencode(topArgs)
+def download_track(jamendo, track_id, track_name, file_format):
 
-    print("Getting top 100 tracks for today")
+    print("Downloading %s" % track_name)
+    dl_request_args = {
+        "audioformat": file_format
+    }
 
-    result = json.load(urllib.urlopen(top100Url))
-    return result
+    filename = track_name + "." + file_format
 
-
-def downloadTrack(trackId, trackName):
-    jamendoStorage = "http://storage.newjamendo.com/"
-    trackArgs = {"trackid": trackId, "format": "ogg2"}
-    trackUrl = jamendoStorage + "?" + urllib.urlencode(trackArgs)
-
-    print("Downloading track")
-    print(trackUrl)
-
-    webFile = urllib.urlopen(trackUrl)
-    localFile = open(trackName, "w")
-
-    localFile.write(webFile.read())
-
-    webFile.close()
-    localFile.close()
+    with open(filename, "w") as fp:
+        raw_response = jamendo.download_track(track_id, dl_request_args)
+        fp.write(raw_response.read())
 
 
 if __name__ == "__main__":
